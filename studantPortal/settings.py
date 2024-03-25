1653bd176525d7761b39f24f102b3b993d16ee74
archu0819 <archanasatpute@hotmail.com>
2024-03-25 11:01:24 +0000

    database added

--- studantPortal/settings.py
+++ studantPortal/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 
 
 
+
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +30,7 @@ TEMPLATES_DIR= BASE_DIR / 'templates'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&*j*&e7r2b36mqrt9wiyn!tjl7p+6)zw_=@wisjuqj)2jvq_8o'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -105,12 +106,12 @@ WSGI_APPLICATION = 'studantPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
